abcb6a0911408d24cf7cd419c3a87bca5355181f
Paul Hayes <fofr@users.noreply.github.com>
2018-04-17 12:02:44 +0100

    Stub some course pages
    * Basic course listing
    * Sign in page
    * Course detail page
    * Edit course parts pages

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Manage teacher training courses',
 
   // Default port that prototype runs on
   port: '3000',
