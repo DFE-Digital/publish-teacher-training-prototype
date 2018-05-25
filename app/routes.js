d49c02979afb9dc4560c25827e5553fdfd98c325
Paul Hayes <fofr@users.noreply.github.com>
2018-05-25 17:12:24 +0100

    Vary course page by subject

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,11 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.get('/course/:subject', function (req, res) {
+  var subject = req.params.subject;
+  res.render('course', { subject: subject.charAt(0).toUpperCase() + subject.slice(1) })
+})
+
 // add your routes here
 
 module.exports = router
