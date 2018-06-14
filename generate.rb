03bb4e5ade68ec1688d7877c282495f18a8280fa
Jake Benilov <benilov@gmail.com>
2018-06-14 12:47:09 +0100

    Update prototype to Catholic Teaching Alliance

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Bromley Schools Collegiate'
+provider = 'Catholic Teaching Alliance (South East London)'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
