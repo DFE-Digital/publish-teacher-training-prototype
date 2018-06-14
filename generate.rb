2df3eda88fb3b7338d309380c0440189393e1708
Jake Benilov <benilov@gmail.com>
2018-06-14 10:30:59 +0100

    Update prototype to Bromley Schools Collegiate

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Swiss Cottage Teaching School Alliance'
+provider = 'Bromley Schools Collegiate'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
