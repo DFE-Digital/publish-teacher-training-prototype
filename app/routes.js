efb1d1096a1ccbb0d07a2e6b466125a0f6618289
Paul Hayes <fofr@users.noreply.github.com>
2018-06-25 14:17:21 +0100

    Duplicate more variant fields

--- app/routes.js
+++ app/routes.js
@@ -18,13 +18,27 @@ router.get('/email-qa-fail/:subject', function (req, res) {
   res.render('email-qa-fail', { subject: subject(req) })
 })
 
-router.get('/course/:subject', function (req, res) {
-  res.render('course', { subject: subject(req) })
+router.get('/course/:accreditor/:subject', function (req, res) {
+  res.render('course', { subject: subject(req), accrediting: accreditor(req) })
 })
 
-router.get('/course/:subject/:view', function (req, res) {
+router.get('/course/:accreditor/:subject/option/:index', function (req, res) {
+  var subj = subject(req);
+
+  res.render(`course/about-this-option`, { accrediting: accreditor(req), subject: subj, option: option(req, subj) })
+})
+
+router.get('/course/:accreditor/:subject/ucas/:code', function (req, res) {
+  var ucasCourse = req.session.data['ucasCourses'].find(function(course) {
+    return course.programmeCode == req.params.code;
+  });
+
+  res.render(`course/from-ucas`, { accrediting: accreditor(req), subject: subject(req), ucasCourse: ucasCourse })
+})
+
+router.get('/course/:accreditor/:subject/:view', function (req, res) {
   var view = req.params.view;
-  res.render(`course/${view}`, { subject: subject(req) })
+  res.render(`course/${view}`, { accrediting: accreditor(req), subject: subject(req) })
 })
 
 router.get('/school/:id', function (req, res) {
@@ -43,14 +57,13 @@ router.get('/school/:id/edit', function (req, res) {
   res.render('edit-school', { school: school })
 })
 
-// add your routes here
-
 function subject(req) {
-  var subject = req.session.data['subjects'].find(function(s) {
+  var accrediting = accreditor(req);
+  var subject = accrediting['subjects'].find(function(s) {
     return s.slug == req.params.subject;
   })
 
-  var folded_course = req.session.data['folded_courses'].find(function(folded_course) {
+  var folded_course = req.session.data['folded_courses'][accrediting['name']].find(function(folded_course) {
     return folded_course.name == subject.name;
   });
 
@@ -58,6 +71,24 @@ function subject(req) {
     name: subject.name,
     slug: subject.slug,
     folded_course: folded_course
+  };
+}
+
+function accreditor(req) {
+  var accreditor = req.session.data['accreditors'].find(function(a) {
+    return a.slug == req.params.accreditor;
+  })
+
+  return accreditor;
+}
+
+function option(req, subject) {
+  var optionIndex = req.params.index - 1;
+  var folded_course = subject.folded_course;
+
+  return {
+    name: folded_course['options'][optionIndex],
+    index: optionIndex
   };
 }
 
