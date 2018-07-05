d9cbec60c6bd1a91547a43316b6b1771b24410f9
Paul Hayes <fofr@users.noreply.github.com>
2018-07-05 19:56:40 +0100

    Switch to University of Derby for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Catholic Teaching Alliance (South East London)'
+provider = 'University of Derby'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
