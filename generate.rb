1f1646300f4615cd8c014ab464b124cff687714a
Paul Hayes <fofr@users.noreply.github.com>
2018-07-06 11:30:58 +0100

    Update for Bracknell test

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'University of Derby'
+provider = 'Bracknell Forest Partnership'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
