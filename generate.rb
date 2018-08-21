989e0af602848f4aedfefc6a0d8aaa2786c3f1ee
Paul Hayes <fofr@users.noreply.github.com>
2018-08-21 16:07:16 +0100

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Bracknell Forest Partnership'
+provider = 'Bromley Schools Collegiate'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
