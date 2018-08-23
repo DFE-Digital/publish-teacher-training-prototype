99d1c2101927a248c15206c2f8592b1b238568c0
Paul Hayes <fofr@users.noreply.github.com>
2018-08-23 07:28:08 +0100

    Disable SimpleMDE

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,36 +16,36 @@ $(document).ready(function () {
       wordCount: true
     })
 
-    $('textarea').each(function() {
-      var $textarea = $(this);
-
-      var simplemde = new SimpleMDE({
-        element: $(this).get(0),
-        autosave: {
-          enabled: true,
-          delay: 1000,
-          uniqueId: $textarea.attr('id')
-        },
-        hideIcons: [
-          'bold',
-          'italic',
-          'heading',
-          'image',
-          'help',
-          'quote'
-        ],
-        spellChecker: false,
-        status: [ {
-          className: "trigger",
-          onUpdate: function(el) {
-            // Make GOV char count work
-            $textarea.blur();
-          }
-        }],
-        forceSync: true,
-        styleSelectedText: false
-      });
-    });
+    // $('textarea').each(function() {
+    //   var $textarea = $(this);
+    //
+    //   var simplemde = new SimpleMDE({
+    //     element: $(this).get(0),
+    //     autosave: {
+    //       enabled: true,
+    //       delay: 1000,
+    //       uniqueId: $textarea.attr('id')
+    //     },
+    //     hideIcons: [
+    //       'bold',
+    //       'italic',
+    //       'heading',
+    //       'image',
+    //       'help',
+    //       'quote'
+    //     ],
+    //     spellChecker: false,
+    //     status: [ {
+    //       className: "trigger",
+    //       onUpdate: function(el) {
+    //         // Make GOV char count work
+    //         $textarea.blur();
+    //       }
+    //     }],
+    //     forceSync: true,
+    //     styleSelectedText: false
+    //   });
+    // });
   });
 
   // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
