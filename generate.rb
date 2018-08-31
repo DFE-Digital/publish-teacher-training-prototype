2b7df04b420d720888709bcde3dbf627f09c4354
Paul Hayes <fofr@users.noreply.github.com>
2018-08-31 11:09:21 +0100

    Use institution with salaried courses

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Angel Islington Teaching School Alliance'
+provider = 'Bitterne Park Teaching School Alliance'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
