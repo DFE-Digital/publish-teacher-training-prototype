d1e2e6d950a8fd84ddf1cb602a6b7b20e631daf1
Paul Hayes <fofr@users.noreply.github.com>
2018-11-15 12:48:46 +0000

    Prep for next session

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'West London Teaching School Alliance (Secondary)'
+provider = 'UCL, University College London (University of London)'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
