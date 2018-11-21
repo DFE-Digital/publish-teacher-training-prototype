84d1e33a05e4947a2df78e730e764bbd0a15790b
Paul Hayes <fofr@users.noreply.github.com>
2018-11-21 12:07:23 +0000

    Revert "Prep for Uni of Herts"
    This reverts commit a7fbe005ef4eb4fb1913240171375403f5f8933d.

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'University of Hertfordshire'
+provider = 'UCL, University College London (University of London)'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
