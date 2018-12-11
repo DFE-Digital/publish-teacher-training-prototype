884668272886ba2b76cb2e1e592cb9b5e9828871
Dan Mitchell <dan@danmitchell.co.uk>
2018-12-11 10:33:50 +0000

    Revert "Prep for research"
    This reverts commit 9ef403926995ecd65557eec340bbf2362dfd6d3f.

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'University of Wolverhampton'
+provider = 'ARK Teacher Training'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
