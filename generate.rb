a82f2c5930638f8b42e4c1cc493bf8c9739f3dbe
Paul Hayes <fofr@users.noreply.github.com>
2018-12-17 16:39:17 +0000

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Launceston College and Duchy Academy Trust Partnership'
+provider = 'The Hampshire LEARN SCITT Partnership'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
