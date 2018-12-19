f3e998ef870a37932b0275722bec8b8d36fa4ce7
Paul Hayes <fofr@users.noreply.github.com>
2018-12-19 11:03:02 +0000

    Revert "Prep for research"
    This reverts commit 6514407ec7328d8c419a12da793f3ca1532f8999.

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Suffolk and Norfolk Primary SCITT'
+provider = 'The Hampshire LEARN SCITT Partnership'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
