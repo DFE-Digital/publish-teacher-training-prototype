f2d92d3e52e945ad10188ae84b4cbf64b9122eb1
Paul Hayes <fofr@users.noreply.github.com>
2019-01-04 08:59:05 +0000

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'The Tommy Flowers SCITT Milton Keynes'
+provider = 'Hillingdon SCITT'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
