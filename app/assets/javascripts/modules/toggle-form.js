beedc73fbeadd9593ba1ea2f7e0fe7150bfbed22
Paul Hayes <fofr@users.noreply.github.com>
2019-01-12 17:49:47 +0000

    Add design for "change" on min reqs

---
+++ app/assets/javascripts/modules/toggle-form.js
@@ -0,0 +1,18 @@
+(function (Modules) {
+  'use strict'
+
+  Modules.ToggleForm = function () {
+    this.start = function ($el) {
+      var toggleSelector = '.js-toggle';
+
+      $el.on('click', toggleSelector, toggle)
+
+      function toggle (event) {
+        $el.find('.js-hidden-form').show();
+        $el.find('.js-hide-on-toggle').hide();
+
+        event.preventDefault()
+      }
+    }
+  }
+})(window.GOVUK.Modules)
