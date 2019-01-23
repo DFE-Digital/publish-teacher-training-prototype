1508aa7e81dd90b03e36a5abaad495ca66da0224
Paul Hayes <fofr@users.noreply.github.com>
2019-01-23 14:59:50 +0000

    Regenerate data

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'The University of Sheffield'
+provider = 'West London Teaching School Alliance (Secondary)'
 courses = data.select {|c| c['provider'] == provider }
 
 # https://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
