9ab867027862d8d63e9cfe55df15e846897f90f2
Paul Hayes <fofr@users.noreply.github.com>
2019-02-21 14:03:57 +0000

    Add a rolled-over flag

--- generate.rb
+++ generate.rb
@@ -14,6 +14,7 @@ all_accredited_bodies = data.map {|c| c['accrediting'] }.uniq.compact.sort
 postcodeRegex =  /([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/
 
 prototype_data = {
+  'rolled-over': false,
   'multi-organisation': false,
   'training-provider-name': provider,
   'provider-code': courses.first['providerCode']
