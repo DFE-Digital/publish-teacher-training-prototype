e50b06bf80ca0235e6de1841b01b1eccc5ec4deb
Paul Hayes <fofr@users.noreply.github.com>
2019-02-28 08:41:47 +0000

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Oxford University'
+provider = 'Cabot Learning Federation SCITT'
 courses = data.select {|c| c['provider'] == provider }
 
 all_accredited_bodies = data.map {|c| c['accrediting'] }.uniq.compact.sort
