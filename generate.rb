ac207846b110d2aedba661de96dbc492a2c1cfac
Paul Hayes <fofr@users.noreply.github.com>
2019-03-01 11:59:32 +0000

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Edge Hill University'
+provider = 'The Northworthy Alliance'
 courses = data.select {|c| c['provider'] == provider }
 
 all_accredited_bodies = data.map {|c| c['accrediting'] }.uniq.compact.sort
