f884ab5172c700280ccfc3de29f86a3509d35fc3
Paul Hayes <fofr@users.noreply.github.com>
2019-03-14 15:54:03 +0000

    Prep for research (next)

--- generate.rb
+++ generate.rb
@@ -5,8 +5,8 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'The Manchester Metropolitan University'
-next_cycle = false
+provider = 'Bath Spa University'
+next_cycle = true
 courses = data.select {|c| c['provider'] == provider }
 
 all_accredited_bodies = data.map {|c| c['accrediting'] }.uniq.compact.sort
