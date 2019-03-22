0e6c5b81b33d3b97d8142d0cff95b847f4612f9e
Paul Hayes <fofr@users.noreply.github.com>
2019-03-22 08:29:17 +0000

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'BSET SCITT'
+provider = 'High Force Education (SCITT)'
 next_cycle = false
 courses = data.select {|c| c['provider'] == provider }
 
