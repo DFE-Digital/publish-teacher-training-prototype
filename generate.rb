86d062c70ca0f0d060f5ea732f3535811d8ce47a
Paul Hayes <fofr@users.noreply.github.com>
2019-03-25 15:23:53 +0000

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Berkshire Teaching School Alliance'
+provider = 'Millais School'
 next_cycle = false
 courses = data.select {|c| c['provider'] == provider }
 
