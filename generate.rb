9b254325a376f5f97a9ecee1bc16c46ada8ddf14
Paul Hayes <fofr@users.noreply.github.com>
2019-04-02 10:55:44 +0100

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Alliance for Learning SCITT'
+provider = 'Royal Academy of Dance'
 next_cycle = false
 courses = data.select {|c| c['provider'] == provider }
 
