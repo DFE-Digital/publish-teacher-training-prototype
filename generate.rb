c78023435af443ac72e39306787cecff73288706
Paul Hayes <fofr@users.noreply.github.com>
2019-04-02 13:59:23 +0100

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Royal Academy of Dance'
+provider = 'Newman University'
 next_cycle = false
 courses = data.select {|c| c['provider'] == provider }
 
