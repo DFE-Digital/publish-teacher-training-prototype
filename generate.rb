6c8fe2c2ca3541ea38cc1d900125683bba3722e0
Paul Hayes <fofr@users.noreply.github.com>
2019-05-29 11:27:43 +0100

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Liverpool Hope University'
+provider = 'East of England Teacher Training Centre'
 next_cycle = false
 courses = data.select {|c| c['provider'] == provider }
 accredited_courses = data.select {|c| c['accrediting'] == provider }
