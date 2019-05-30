80dbf9065de36376838282d677784f5772244029
Paul Hayes <fofr@users.noreply.github.com>
2019-05-30 14:20:54 +0100

    Enable rollover mode

--- generate.rb
+++ generate.rb
@@ -6,7 +6,7 @@ require 'json'
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
 provider = 'East of England Teacher Training Centre'
-next_cycle = false
+next_cycle = true
 courses = data.select {|c| c['provider'] == provider }
 accredited_courses = data.select {|c| c['accrediting'] == provider }
 isAccreditedBody = !courses.first['route'].include?('School Direct')
@@ -17,7 +17,7 @@ all_accredited_bodies = data.map {|c| c['accrediting'] }.uniq.compact.sort
 postcodeRegex =  /([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/
 
 prototype_data = {
-  'rolled-over': false,
+  'rolled-over': true,
   'next-cycle': next_cycle,
   'multi-organisation': false,
   'ucas-gt12': 'Applicants must confirm their place',
