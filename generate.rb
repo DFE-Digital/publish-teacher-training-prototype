958724b369035f7669c028eab67edc75decf0b39
Paul Hayes <fofr@users.noreply.github.com>
2019-06-03 16:45:11 +0100

    Prep for research

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'University of Portsmouth'
+provider = 'University of Bristol'
 next_cycle = false
 courses = data.select {|c| c['provider'] == provider }
 accredited_courses = data.select {|c| c['accrediting'] == provider }
