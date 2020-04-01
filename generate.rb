2610104f642ad880b919b71b315225fb8d4ebcdc
Jesse Yuen <hello@jesseyuen.com>
2020-04-01 11:14:25 +0100

    data update

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Kingston University'
+provider = 'University of Winchester'
 next_cycle = false
 courses = data.select {|c| c['provider'] == provider }
 accredited_courses = data.select {|c| c['accrediting'] == provider }
