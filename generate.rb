f460c821248caf1ee82b742406f684d5668c028c
Paul Hayes <fofr@users.noreply.github.com>
2020-05-19 12:28:14 +0100

    Revert "Prep for research"
    This reverts commit 5099013d682a32bbb82d58c5c6e018784fb98d84.

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'University of Bedfordshire'
+provider = 'Oxford Brookes University'
 next_cycle = false
 courses = data.select {|c| c['provider'] == provider }
 accredited_courses = data.select {|c| c['accrediting'] == provider }
