3da719cd1bd2a2c14b14f7bb45f1e640613d4af5
Jesse Yuen <hello@jesseyuen.com>
2021-04-27 13:35:39 +0100

    Bug fix on org name

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Beech Hill'
+provider = 'Beech Hill Primary School'
 next_cycle = false
 courses = data.select {|c| c['provider'] == provider }
 accredited_courses = data.select {|c| c['accrediting'] == provider }
