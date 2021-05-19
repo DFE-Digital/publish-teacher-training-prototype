f218112887da4225d46df45ec023f9e71d9da9d2
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-05-19 12:56:05 +0100

    Seed prototype with data from The University of York

--- generate.rb
+++ generate.rb
@@ -5,8 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-# provider = 'University of Bristol'
-provider = 'Red Kite Teacher Training'
+provider = 'The University of York'
 next_cycle = true
 courses = data.select {|c| c['provider'] == provider }
 accredited_courses = data.select {|c| c['accrediting'] == provider }
