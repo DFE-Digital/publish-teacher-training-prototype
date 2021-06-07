ca1ae0bf752516e3a70248297e15c9250dc795eb
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-06-07 16:48:21 +0100

    Use Gorse SCITT as provider

--- generate.rb
+++ generate.rb
@@ -5,8 +5,8 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'UCL, University College London (University of London)'
-# provider = 'Gorse SCITT'
+# provider = 'UCL, University College London (University of London)'
+provider = 'Gorse SCITT'
 next_cycle = true
 courses = data.select {|c| c['provider'] == provider }
 abort("No courses found - check the provider name exactly matches one in JSON file") if courses.length.zero?
