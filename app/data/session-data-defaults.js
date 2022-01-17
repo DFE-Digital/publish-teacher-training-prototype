423b9b0764c0c0118bf937d0463a229a59f8cecf
Simon Whatley <simonwhatley@gmail.com>
2022-01-17 13:07:10 +0000

    Update session-data-defaults.js

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,212 +1,214 @@
-const prototypeData = require('./prototype-data')
+// const prototypeData = require('./prototype-data')
+//
+// prototypeData.uploadedLocations = [{
+//   urn: 115723,
+//   type: 'school',
+//   name: 'Archway School',
+//   'address-line1': 'Paganhill',
+//   'address-level2': 'Stroud',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL5 4AX'
+// }, {
+//   urn: 115720,
+//   type: 'school',
+//   name: 'Barnwood Park School',
+//   'address-line1': 'St Lawrence Road',
+//   'address-line2': 'Barnwood',
+//   'address-level2': 'Gloucester',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL4 3QU'
+// }, {
+//   urn: 136725,
+//   type: 'school',
+//   name: 'Cheltenham Bournside School and Sixth Form Centre',
+//   'address-line1': 'Warden Hill Road',
+//   'address-level2': 'Cheltenham',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL51 3EF'
+// }, {
+//   urn: 136960,
+//   type: 'school',
+//   name: 'Chipping Campden School',
+//   'address-line1': 'Cider Mill Lane',
+//   'address-level2': 'Chipping Campden',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL55 6HU'
+// }, {
+//   urn: 136623,
+//   type: 'school',
+//   name: 'Chosen Hill School',
+//   'address-line1': 'Brookfield Road',
+//   'address-line2': 'Churchdown',
+//   'address-level2': 'Gloucester',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL3 2PL'
+// }, {
+//   urn: 137634,
+//   type: 'school',
+//   name: 'Churchdown School',
+//   'address-line1': 'Winston Road',
+//   'address-line2': 'Churchdown',
+//   'address-level2': 'Gloucester',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL3 2RB'
+// }, {
+//   urn: 136527,
+//   type: 'school',
+//   name: 'Cirencester Deer Park School',
+//   'address-line1': 'Stroud Road',
+//   'address-level2': 'Cirencester',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL7 1XB'
+// }, {
+//   urn: 137217,
+//   type: 'school',
+//   name: 'Cirencester Kingshill School',
+//   'address-line1': 'Kingshill Lane',
+//   'address-level2': 'Cirencester',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL7 1HS'
+// }, {
+//   urn: 136772,
+//   type: 'school',
+//   name: 'Cleeve School',
+//   'address-line1': 'Two Hedges Road',
+//   'address-line2': 'Bishops Cleeve',
+//   'address-level2': 'Cheltenham',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL52 8AE'
+// }, {
+//   urn: 137387,
+//   type: 'school',
+//   name: 'Dene Magna School',
+//   'address-line1': 'Abenhall Road',
+//   'address-level2': 'Mitcheldean',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL17 0DU'
+// }, {
+//   urn: 137097,
+//   type: 'school',
+//   name: 'Farmor’s School',
+//   'address-line1': 'The Park',
+//   'address-level2': 'Fairford',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL7 4JQ'
+// }, {
+//   urn: 137033,
+//   type: 'school',
+//   name: 'Katharine Lady Berkeley’s School',
+//   'address-line1': 'Kingswood Road',
+//   'address-level2': 'Wotton-under-Edge',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL12 8RB'
+// }, {
+//   urn: 115775,
+//   type: 'school',
+//   name: 'Maidenhill School',
+//   'address-line1': 'Kings Road',
+//   'address-level2': 'Stonehouse',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL10 2HA'
+// }, {
+//   urn: 138746,
+//   type: 'school',
+//   name: 'Newent Community School',
+//   'address-line1': 'Watery Lane',
+//   'address-level2': 'Newent',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL18 1QF'
+// }, {
+//   urn: 115772,
+//   type: 'school',
+//   name: 'Pittville School',
+//   'address-line1': 'Albert Road',
+//   'address-level2': 'Cheltenham',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL52 3JD'
+// }, {
+//   urn: 115758,
+//   type: 'school',
+//   name: 'Rednock School',
+//   'address-line1': 'Kingshill Road',
+//   'address-level2': 'Dursley',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL11 4BY'
+// }, {
+//   urn: 136767,
+//   type: 'school',
+//   name: 'Ribston Hall High School',
+//   'address-line1': 'Stroud Road',
+//   'address-level2': 'Gloucester',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL1 5LE'
+// }, {
+//   urn: 137298,
+//   type: 'school',
+//   name: 'Severn Vale School',
+//   'address-line1': 'School Lane',
+//   'address-line2': 'Quedgeley',
+//   'address-level2': 'Gloucester',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL2 4PR'
+// }, {
+//   urn: 136306,
+//   type: 'school',
+//   name: 'Sir Thomas Rich’s School',
+//   'address-line1': 'Oakleaze',
+//   'address-level2': 'Gloucester',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL2 0LF'
+// }, {
+//   urn: 136982,
+//   type: 'school',
+//   name: 'St Peter’s Catholic High School and Sixth Form Centre',
+//   'address-line1': 'Stroud Road',
+//   'address-line2': 'Tuffley',
+//   'address-level2': 'Gloucester',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL4 0DD'
+// }, {
+//   urn: 136874,
+//   type: 'school',
+//   name: 'Stroud High School',
+//   'address-line1': 'Cainscross Road',
+//   'address-level2': 'Stroud',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL5 4HF'
+// }, {
+//   urn: 137752,
+//   type: 'school',
+//   name: 'Tewkesbury School',
+//   'address-line1': 'Ashchurch Road',
+//   'address-level2': 'Tewkesbury',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL20 8DF'
+// }, {
+//   urn: 136578,
+//   type: 'school',
+//   name: 'The Crypt School',
+//   'address-line1': 'Podsmead Road',
+//   'address-level2': 'Gloucester',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL2 5AE'
+// }, {
+//   urn: 137059,
+//   type: 'school',
+//   name: 'Thomas Keble School',
+//   'address-line1': 'Eastcombe',
+//   'address-level2': 'Stroud',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'GL6 7DY'
+// }, {
+//   urn: 137382,
+//   type: 'school',
+//   name: 'Wyedean School and Sixth Form Centre',
+//   'address-line1': 'Beachley Road',
+//   'address-line2': 'Sedbury',
+//   'address-level2': 'Chepstow',
+//   'address-level1': 'Gloucestershire',
+//   'postal-code': 'NP16 7AA'
+// }]
 
-prototypeData.uploadedLocations = [{
-  urn: 115723,
-  type: 'school',
-  name: 'Archway School',
-  'address-line1': 'Paganhill',
-  'address-level2': 'Stroud',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL5 4AX'
-}, {
-  urn: 115720,
-  type: 'school',
-  name: 'Barnwood Park School',
-  'address-line1': 'St Lawrence Road',
-  'address-line2': 'Barnwood',
-  'address-level2': 'Gloucester',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL4 3QU'
-}, {
-  urn: 136725,
-  type: 'school',
-  name: 'Cheltenham Bournside School and Sixth Form Centre',
-  'address-line1': 'Warden Hill Road',
-  'address-level2': 'Cheltenham',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL51 3EF'
-}, {
-  urn: 136960,
-  type: 'school',
-  name: 'Chipping Campden School',
-  'address-line1': 'Cider Mill Lane',
-  'address-level2': 'Chipping Campden',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL55 6HU'
-}, {
-  urn: 136623,
-  type: 'school',
-  name: 'Chosen Hill School',
-  'address-line1': 'Brookfield Road',
-  'address-line2': 'Churchdown',
-  'address-level2': 'Gloucester',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL3 2PL'
-}, {
-  urn: 137634,
-  type: 'school',
-  name: 'Churchdown School',
-  'address-line1': 'Winston Road',
-  'address-line2': 'Churchdown',
-  'address-level2': 'Gloucester',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL3 2RB'
-}, {
-  urn: 136527,
-  type: 'school',
-  name: 'Cirencester Deer Park School',
-  'address-line1': 'Stroud Road',
-  'address-level2': 'Cirencester',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL7 1XB'
-}, {
-  urn: 137217,
-  type: 'school',
-  name: 'Cirencester Kingshill School',
-  'address-line1': 'Kingshill Lane',
-  'address-level2': 'Cirencester',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL7 1HS'
-}, {
-  urn: 136772,
-  type: 'school',
-  name: 'Cleeve School',
-  'address-line1': 'Two Hedges Road',
-  'address-line2': 'Bishops Cleeve',
-  'address-level2': 'Cheltenham',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL52 8AE'
-}, {
-  urn: 137387,
-  type: 'school',
-  name: 'Dene Magna School',
-  'address-line1': 'Abenhall Road',
-  'address-level2': 'Mitcheldean',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL17 0DU'
-}, {
-  urn: 137097,
-  type: 'school',
-  name: 'Farmor’s School',
-  'address-line1': 'The Park',
-  'address-level2': 'Fairford',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL7 4JQ'
-}, {
-  urn: 137033,
-  type: 'school',
-  name: 'Katharine Lady Berkeley’s School',
-  'address-line1': 'Kingswood Road',
-  'address-level2': 'Wotton-under-Edge',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL12 8RB'
-}, {
-  urn: 115775,
-  type: 'school',
-  name: 'Maidenhill School',
-  'address-line1': 'Kings Road',
-  'address-level2': 'Stonehouse',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL10 2HA'
-}, {
-  urn: 138746,
-  type: 'school',
-  name: 'Newent Community School',
-  'address-line1': 'Watery Lane',
-  'address-level2': 'Newent',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL18 1QF'
-}, {
-  urn: 115772,
-  type: 'school',
-  name: 'Pittville School',
-  'address-line1': 'Albert Road',
-  'address-level2': 'Cheltenham',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL52 3JD'
-}, {
-  urn: 115758,
-  type: 'school',
-  name: 'Rednock School',
-  'address-line1': 'Kingshill Road',
-  'address-level2': 'Dursley',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL11 4BY'
-}, {
-  urn: 136767,
-  type: 'school',
-  name: 'Ribston Hall High School',
-  'address-line1': 'Stroud Road',
-  'address-level2': 'Gloucester',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL1 5LE'
-}, {
-  urn: 137298,
-  type: 'school',
-  name: 'Severn Vale School',
-  'address-line1': 'School Lane',
-  'address-line2': 'Quedgeley',
-  'address-level2': 'Gloucester',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL2 4PR'
-}, {
-  urn: 136306,
-  type: 'school',
-  name: 'Sir Thomas Rich’s School',
-  'address-line1': 'Oakleaze',
-  'address-level2': 'Gloucester',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL2 0LF'
-}, {
-  urn: 136982,
-  type: 'school',
-  name: 'St Peter’s Catholic High School and Sixth Form Centre',
-  'address-line1': 'Stroud Road',
-  'address-line2': 'Tuffley',
-  'address-level2': 'Gloucester',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL4 0DD'
-}, {
-  urn: 136874,
-  type: 'school',
-  name: 'Stroud High School',
-  'address-line1': 'Cainscross Road',
-  'address-level2': 'Stroud',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL5 4HF'
-}, {
-  urn: 137752,
-  type: 'school',
-  name: 'Tewkesbury School',
-  'address-line1': 'Ashchurch Road',
-  'address-level2': 'Tewkesbury',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL20 8DF'
-}, {
-  urn: 136578,
-  type: 'school',
-  name: 'The Crypt School',
-  'address-line1': 'Podsmead Road',
-  'address-level2': 'Gloucester',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL2 5AE'
-}, {
-  urn: 137059,
-  type: 'school',
-  name: 'Thomas Keble School',
-  'address-line1': 'Eastcombe',
-  'address-level2': 'Stroud',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'GL6 7DY'
-}, {
-  urn: 137382,
-  type: 'school',
-  name: 'Wyedean School and Sixth Form Centre',
-  'address-line1': 'Beachley Road',
-  'address-line2': 'Sedbury',
-  'address-level2': 'Chepstow',
-  'address-level1': 'Gloucestershire',
-  'postal-code': 'NP16 7AA'
-}]
+let defaults = {}
 
-// module.exports = prototypeData
+module.exports = defaults
