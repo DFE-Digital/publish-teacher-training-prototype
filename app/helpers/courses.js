f73a52c26560a69fe31f946e6abe581f21bc18ba
Simon Whatley <simonwhatley@gmail.com>
2022-01-20 10:11:47 +0000

    Add helpers

---
+++ app/helpers/courses.js
@@ -0,0 +1,26 @@
+const STUDY_MODES = {
+  full_time: "Full time", // "F" in the live service
+  part_time: "Part time", // "P" in the live service
+  both: "Full time and part time" // "B" in the live service
+}
+
+const PROGRAMME_TYPES = {
+  // higher_education_programme: "HE",
+  // school_direct_training_programme: "SD",
+  // school_direct_salaried_training_programme: "SS",
+  // scitt_programme: "SC",
+  // pg_teaching_apprenticeship: "TA"
+}
+
+const LEVELS = {
+  primary: "Primary",
+  secondary: "Secondary",
+  further_education: "Further education"
+}
+
+const DEGREE_GRADES = {
+  0: "2:1 or above, or equivalent",
+  1: "2:2 or above, or equivalent",
+  2: "Third or above, or equivalent",
+  9: "Not required"
+}
---
+++ app/helpers/locations.js
@@ -0,0 +1,6 @@
+const STATUSES = {
+  discontinued: "Discontinued", // "D" in the live service
+  new: "New", // "N" in the live service
+  running: "Running", // "R" in the live service
+  suspended: "Suspended" // "S" in the live service
+}
