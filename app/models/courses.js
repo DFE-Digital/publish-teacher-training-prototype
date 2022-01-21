9ae5c2a6c9ef225f498c0a237c79d35ab8e6d927
Simon Whatley <simonwhatley@gmail.com>
2022-01-21 17:57:22 +0000

    Add model stubs

---
+++ app/models/courses.js
@@ -0,0 +1,31 @@
+exports.find = (data) => {
+
+}
+
+exports.findOne = (id) => {
+
+}
+
+exports.insertOne = (data) => {
+
+}
+
+exports.insertMany = (data) => {
+
+}
+
+exports.updateOne = (id, data) => {
+
+}
+
+exports.updateMany = (data) => {
+
+}
+
+exports.deleteOne = (id) => {
+
+}
+
+exports.deleteMany = (data) => {
+
+}
