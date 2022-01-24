e5bd59a81864c3a3ca7d16544cfd43d7b59438e3
Simon Whatley <simonwhatley@gmail.com>
2022-01-24 20:11:35 +0000

    Update course helper to use data

--- app/helpers/courses.js
+++ app/helpers/courses.js
@@ -1,26 +1,92 @@
-const STUDY_MODES = {
-  full_time: "Full time", // "F" in the live service
-  part_time: "Part time", // "P" in the live service
-  both: "Full time and part time" // "B" in the live service
+// const DEGREE_GRADES = {
+//   0: "2:1 or above, or equivalent",
+//   1: "2:2 or above, or equivalent",
+//   2: "Third or above, or equivalent",
+//   9: "Not required"
+// }
+
+exports.getAgeRangeOptions = (subjectLevel = 'secondary', selectedItem) => {
+  const items = []
+
+  let ageRanges = require('../data/age-ranges')
+  if (subjectLevel) {
+    ageRanges = ageRanges.filter(range => range.level === subjectLevel)
+  }
+
+  ageRanges.forEach((ageRange, i) => {
+    const item = {}
+
+    item.text = ageRange.name
+    item.value = ageRange.code
+    item.id = ageRange.id
+    item.checked = (selectedItem && selectedItem.includes(ageRange.code)) ? 'checked' : ''
+
+    items.push(item)
+  })
+
+  items.sort((a,b) => {
+    return a.text.localeCompare(b.text)
+  })
+
+  return items
 }
 
-const PROGRAMME_TYPES = {
-  // higher_education_programme: "HE",
-  // school_direct_training_programme: "SD",
-  // school_direct_salaried_training_programme: "SS",
-  // scitt_programme: "SC",
-  // pg_teaching_apprenticeship: "TA"
+exports.getQualificationOptions = (selectedItem) => {
+  const items = []
+  const qualifications = []
+
+  qualifications.forEach((qualification, i) => {
+    const item = {}
+
+    item.text = qualification.name
+    item.value = qualification.code
+    item.id = qualification.id
+    item.checked = (selectedItem && selectedItem.includes(qualification.code)) ? 'checked' : ''
+
+    items.push(item)
+  })
+
+  return items
 }
 
-const LEVELS = {
-  primary: "Primary",
-  secondary: "Secondary",
-  further_education: "Further education"
+// TODO: funding type is dependent on provider type -> programType
+// higher_education_programme: "HE",
+// school_direct_training_programme: "SD",
+// school_direct_salaried_training_programme: "SS",
+// scitt_programme: "SC",
+// pg_teaching_apprenticeship: "TA"
+exports.getFundingTypeOptions = (selectedItem) => {
+  const items = []
+  const fundingTypes = require('../data/funding-types')
+
+  fundingTypes.forEach((fundingType, i) => {
+    const item = {}
+
+    item.text = fundingType.name
+    item.value = fundingType.code
+    item.id = fundingType.id
+    item.checked = (selectedItem && selectedItem.includes(fundingType.code)) ? 'checked' : ''
+
+    items.push(item)
+  })
+
+  return items
 }
 
-const DEGREE_GRADES = {
-  0: "2:1 or above, or equivalent",
-  1: "2:2 or above, or equivalent",
-  2: "Third or above, or equivalent",
-  9: "Not required"
+exports.getStudyModeOptions = (selectedItem) => {
+  const items = []
+  const studyModes = require('../data/study-modes')
+
+  studyModess.forEach((studyModes, i) => {
+    const item = {}
+
+    item.text = studyModes.name
+    item.value = studyModes.code
+    item.id = studyModes.id
+    item.checked = (selectedItem && selectedItem.includes(studyModes.code)) ? 'checked' : ''
+
+    items.push(item)
+  })
+
+  return items
 }
