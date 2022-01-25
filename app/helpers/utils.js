0a4afe9cd5f05c4ce19218d5a9a80742ba260d60
Simon Whatley <simonwhatley@gmail.com>
2022-01-25 15:08:33 +0000

    Add utils helper

---
+++ app/helpers/utils.js
@@ -0,0 +1,22 @@
+exports.arrayToList = (array, join = ', ', final = ' and ') => {
+  const arr = array.slice(0)
+
+  const last = arr.pop()
+
+  if (array.length > 1) {
+    return arr.join(join) + final + last
+  }
+
+  return last
+}
+
+exports.slugify = (text) => {
+  return text.trim()
+    .toLowerCase()
+    // remove non-word [a-z0-9_], non-whitespace, non-hyphen characters
+    .replace(/[^\w\s-]/g, '')
+    // swap any length of whitespace, underscore, hyphen characters with a single -
+    .replace(/[\s_-]+/g, '-')
+    // remove leading, trailing -
+    .replace(/^-+|-+$/g, '')
+  }
