2b6b090b85b9d0f308ee2534703334818a9a42ad
Simon Whatley <simonwhatley@gmail.com>
2022-01-26 14:34:25 +0000

    Update session-data-defaults.js

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,11 +1,11 @@
 let defaults = {}
 
-defaults.courses = require('./courses')
-defaults.courseLocations = require('./course-locations')
-defaults.courseSubjects = require('./course-subjects')
-defaults.locations = require('./locations')
-defaults.organisations = require('./organisations')
-defaults.subjects = require('./subjects')
-defaults.users = require('./users')
+// defaults.courses = require('./courses')
+// defaults.courseLocations = require('./course-locations')
+// defaults.courseSubjects = require('./course-subjects')
+// defaults.locations = require('./locations')
+// defaults.organisations = require('./organisations')
+// defaults.subjects = require('./subjects')
+// defaults.users = require('./users')
 
 module.exports = defaults
