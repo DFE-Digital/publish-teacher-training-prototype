28529ecdbb3c5c78ce0c4d8cbbbacb92f3b3c4f5
Simon Whatley <simonwhatley@gmail.com>
2022-02-08 10:03:09 +0000

    Find subject by code or ID

--- app/models/subjects.js
+++ app/models/subjects.js
@@ -4,6 +4,13 @@ exports.find = (params) => {
 
 exports.findOne = (params) => {
   const subjects = require('../data/subjects')
-  const subject = subjects.find(subject => subject.id === params.subjectId)
+
+  let subject
+  if (params.subjectId) {
+    subject = subjects.find(subject => subject.id === params.subjectId)
+  } else {
+    subject = subjects.find(subject => subject.code === params.subjectCode)
+  }
+
   return subject
 }
