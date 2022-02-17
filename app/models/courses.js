6662628d9a05126d666443e7fc0d4df127b3e511
Simon Whatley <simonwhatley@gmail.com>
2022-02-17 10:47:58 +0000

    Update insertOne course function

--- app/models/courses.js
+++ app/models/courses.js
@@ -53,13 +53,148 @@ exports.insertOne = (params) => {
 
     let course = {}
 
+    course.id = uuid()
 
+    if (params.course.name) {
+      course.name = params.course.name
+    }
+
+    if (params.course.code) {
+      course.code = params.course.code
+    }
+
+    if (params.course.subjectLevel) {
+      course.subjectLevel = params.course.subjectLevel
+    }
+
+    if (params.course.isSend) {
+      course.isSend = params.course.isSend
+    }
+
+    if (params.course.subjects) {
+      const subjects = []
+
+      params.course.subjects.forEach((courseSubject, i) => {
+        const subject = {}
+
+        const s = subjectModel.findOne({ subjectCode: courseSubject })
+
+        subject.id = s.id
+        subject.code = s.code
+        subject.name = s.name
+
+        subjects.push(subject)
+      })
+
+      course.subjects = subjects
+
+    }
+
+    if (params.course.ageRange) {
+      course.ageRange = params.course.ageRange
+
+      // handle 'other' age ranges
+      if (params.course.ageRange === 'other') {
+        course.ageRangeOther = {}
+        course.ageRangeOther.from = params.course.ageRangeOther.from
+        course.ageRangeOther.to = params.course.ageRangeOther.to
+      }
+    }
+
+    if (params.course.qualification) {
+      course.qualification = params.course.qualification
+    }
+
+    if (params.course.fundingType) {
+      course.fundingType = params.course.fundingType
+
+      // update programType based on organisation type and funding type
+      const tp = organisationModel.findOne({ organisationId: params.organisationId })
+
+      if (tp.type === 'lead_school') {
+        if (params.course.fundingType === 'fee') {
+          course.programType = 'SD'
+        }
+
+        if (params.course.fundingType === 'salary') {
+          course.programType = 'SS'
+        }
+
+        if (params.course.fundingType === 'apprenticeship') {
+          course.programType = 'TA'
+        }
+      }
+
+      if (tp.type === 'scitt') {
+        course.programType = 'SC'
+      }
+
+      if (tp.type === 'hei') {
+        course.programType = 'HE'
+      }
+
+    }
+
+    if (params.course.studyMode) {
+      course.studyMode = params.course.studyMode
+    }
+
+    if (params.course.locations) {
+      const locations = []
+
+      params.course.locations.forEach((courseLocation, i) => {
+        const location = {}
+
+        const cl = locationModel.findOne({ organisationId: params.organisationId, locationId: courseLocation })
+
+        location.id = cl.id
+        location.name = cl.name
+
+        locations.push(location)
+      })
+
+      course.locations = locations
+    }
+
+    if (params.organisationId) {
+      const tp = organisationModel.findOne({ organisationId: params.organisationId })
+
+      course.trainingProvider = {}
+      course.trainingProvider.id = tp.id
+      course.trainingProvider.code = tp.code
+      course.trainingProvider.name = tp.name
+    }
+
+    if (params.course.accreditedBody) {
+      const ab = organisationModel.findOne({ organisationId: params.course.accreditedBody })
+
+      course.accreditedBody = {}
+      course.accreditedBody.id = ab.id
+      course.accreditedBody.code = ab.code
+      course.accreditedBody.name = ab.name
+    }
+
+    if (params.course.startDate) {
+      course.startDate = params.course.startDate
+    }
+
+    if (params.course.applicationsOpenDate) {
+      course.applicationsOpenDate = params.course.applicationsOpenDate
+
+      // handle 'other' applications open dates
+      if (params.course.applicationsOpenDate === 'other') {
+        course.applicationsOpenDateOther = params.course.applicationsOpenDateOther
+      }
+    }
+
+    // draft
+    course.status = '0'
 
     course.createdAt = new Date()
 
     const directoryPath = path.join(__dirname, '../data/courses/' + params.organisationId)
 
-    const filePath = directoryPath + '/' + params.courseId + '.json'
+    const filePath = directoryPath + '/' + course.id + '.json'
 
     // create a JSON sting for the submitted data
     const fileData = JSON.stringify(course)
