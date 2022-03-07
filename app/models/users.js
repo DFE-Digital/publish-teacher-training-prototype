4a6be64b25b66875c597b8d569f5b8712d8175c0
Simon Whatley <simonwhatley@gmail.com>
2022-03-07 17:40:38 +0000

    Add user model

---
+++ app/models/users.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const fs = require('fs')
+const { v4: uuid } = require('uuid')
+
+exports.find = (params) => {
+  const users = require('../data/users')
+
+  if (params.organisationId) {
+    users = users.filter(user => {
+      return user.organisations.find(organisation => organisation.id === params.organisationId)
+    })
+  }
+
+  if (params.userId) {
+    users = users.find(user => user.id === params.userId)
+  }
+
+  return users
+}
+
+exports.findOne = (params) => {
+  let user = {}
+
+  if (params.organisationId && params.userId) {
+    user = this.findOne({
+      organisationId: params.organisationId,
+      userId: params.userId
+    })
+  }
+
+  return user
+}
+
+exports.insertOne = (params) => {
+
+}
+
+exports.updateOne = (params) => {
+
+}
