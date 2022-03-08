479b98b3418e690b28383eb7df1ed1c7e965bcef
Simon Whatley <simonwhatley@gmail.com>
2022-03-08 12:01:12 +0000

    Update user model insertOne and updateOne functions

--- app/models/users.js
+++ app/models/users.js
@@ -2,6 +2,8 @@ const path = require('path')
 const fs = require('fs')
 const { v4: uuid } = require('uuid')
 
+const organisationModel = require('./organisations')
+
 exports.find = (params) => {
   let users = require('../data/users')
 
@@ -32,11 +34,79 @@ exports.findOne = (params) => {
 }
 
 exports.insertOne = (params) => {
+  let user = {}
 
+  if (params.organisationId) {
+    user.id = uuid()
+
+    if (params.user.firstName) {
+      user.firstName = params.user.firstName
+    }
+
+    if (params.user.lastName) {
+      user.lastName = params.user.lastName
+    }
+
+    if (params.user.email) {
+      user.email = params.user.email
+    }
+
+    user.organisations = []
+
+    const o = organisationModel.findOne({ organisationId: params.organisationId })
+
+    const organisation = {}
+    organisation.id = o.id
+    organisation.code = o.code
+    organisation.name = o.name
+    organisation.roles = []
+
+    user.organisations.push(organisation)
+
+    user.createdAt = new Date()
+
+    // const directoryPath = path.join(__dirname, '../data/users/')
+    //
+    // const filePath = directoryPath + '/' + user.id + '.json'
+    //
+    // // create a JSON sting for the submitted data
+    // const fileData = JSON.stringify(user)
+    //
+    // // write the JSON data
+    // fs.writeFileSync(filePath, fileData)
+  }
+
+  return user
 }
 
 exports.updateOne = (params) => {
+  if (params.organisationId && params.userId) {
+    let user = this.findOne({ organisationId: params.organisationId, userId: params.userId })
 
+    if (params.user.firstName) {
+      user.firstName = params.user.firstName
+    }
+
+    if (params.user.lastName) {
+      user.lastName = params.user.lastName
+    }
+
+    if (params.user.email) {
+      user.email = params.user.email
+    }
+
+    user.updatedAt = new Date()
+
+    // const directoryPath = path.join(__dirname, '../data/users/')
+    //
+    // const filePath = directoryPath + '/' + params.userId + '.json'
+    //
+    // // create a JSON sting for the submitted data
+    // const fileData = JSON.stringify(user)
+    //
+    // // write the JSON data
+    // fs.writeFileSync(filePath, fileData)
+  }
 }
 
 exports.deleteOne = (params) => {
