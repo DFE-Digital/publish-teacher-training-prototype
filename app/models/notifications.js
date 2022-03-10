3a22ebdd1099c97cb7bb629b3b6d6212f5a1daca
Simon Whatley <simonwhatley@gmail.com>
2022-03-10 14:17:15 +0000

    Add notifications model

---
+++ app/models/notifications.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const fs = require('fs')
+
+const userModel = require('./users')
+
+exports.updateOne = (params) => {
+  if (params.organisationId && params.userId) {
+    let user = userModel.findOne({ organisationId: params.organisationId, userId: params.userId })
+
+    user.organisations.forEach((organisation, i) => {
+
+      if (organisation.id === params.organisationId) {
+        organisation.notifications = params.notifications
+      }
+
+    })
+
+    user.updatedAt = new Date()
+
+    const directoryPath = path.join(__dirname, '../data/users/')
+
+    const filePath = directoryPath + '/' + user.id + '.json'
+
+    // create a JSON sting for the submitted data
+    const fileData = JSON.stringify(user)
+
+    // write the JSON data
+    fs.writeFileSync(filePath, fileData)
+  }
+}
