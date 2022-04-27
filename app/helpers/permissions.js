cfe3c9b9d7f7ad897e6636e54a12e2056e48efe3
Simon Whatley <simonwhatley@gmail.com>
2022-04-27 21:33:33 +0100

    Add permissions helper

---
+++ app/helpers/permissions.js
@@ -0,0 +1,30 @@
+const permissions = require('../data/permission-types')
+
+exports.getPermissionsOptions = (selectedItem) => {
+  const items = []
+
+  permissions.forEach((permission, i) => {
+    const item = {}
+
+    item.text = permission.name
+    item.value = permission.code
+    item.id = permission.id
+    item.checked = (selectedItem && selectedItem.includes(permission.code)) ? 'checked' : ''
+
+    items.push(item)
+  })
+
+  return items
+}
+
+exports.getPermissionLabel = (code) => {
+  const permission = permissions.find(permission => permission.code === code)
+
+  let label = code
+
+  if (permission) {
+    label = permission.name
+  }
+
+  return label
+}
