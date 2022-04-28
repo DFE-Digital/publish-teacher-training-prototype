8ee09829f51fa6d21b3b9d6a0b7c6a464a534656
Simon Whatley <simonwhatley@gmail.com>
2022-04-28 12:08:39 +0100

    Update permissions model

--- app/models/permissions.js
+++ app/models/permissions.js
@@ -1,25 +1,24 @@
 const path = require('path')
 const fs = require('fs')
 
-const organisationModel = require('./organisations')
-
 const directoryPath = path.join(__dirname, '../data/organisations/')
 
-exports.updateOne = (params) => {
-  if (params.organisationId) {
-    let organisation = organisationModel.findOne({ organisationId: params.organisationId })
+exports.findOne = (params) => {
+  let organisation = {}
+  let permissions = []
 
-    // permissions
+  if (params.trainingPartnerId && params.accreditedBodyId) {
+    const filePath = directoryPath + '/' + params.trainingPartnerId + '.json'
 
+    let raw = fs.readFileSync(filePath)
+    organisation = JSON.parse(raw)
 
-    organisation.updatedAt = new Date()
-
-    const filePath = directoryPath + '/' + organisation.id + '.json'
-
-    // create a JSON sting for the submitted data
-    const fileData = JSON.stringify(organisation)
-
-    // write the JSON data
-    fs.writeFileSync(filePath, fileData)
+    if (organisation.accreditedBodies) {
+      permissions = organisation.accreditedBodies.find(
+        accreditedBody => accreditedBody.id === params.accreditedBodyId
+      ).permissions
+    }
   }
+
+  return permissions
 }
