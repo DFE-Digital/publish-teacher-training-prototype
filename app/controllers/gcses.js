f3bcb2ad00fceb1ca3e11030c5525fef022dd22a
Simon Whatley <simonwhatley@gmail.com>
2022-06-06 11:23:04 +0100

    Add error messaging

--- app/controllers/gcses.js
+++ app/controllers/gcses.js
@@ -23,6 +23,58 @@ exports.edit_gcses_post = (req, res) => {
 
   const errors = []
 
+  if (!req.session.data.course.acceptPendingGcse) {
+    const error = {};
+    error.fieldName = "gcse-accept-pending";
+    error.href = "#gcse-accept-pending";
+    error.text = "Select if you will consider candidates with pending GCSEs";
+    errors.push(error);
+  } else {
+    course.acceptPendingGcse = req.session.data.course.acceptPendingGcse
+  }
+
+  if (!req.session.data.course.acceptGcseEquivalency) {
+    const error = {};
+    error.fieldName = "gcse-accept-equivalency";
+    error.href = "#gcse-accept-equivalency";
+    error.text = "Select if you will consider candidates who need to take an equivalency test in English, maths or science";
+    errors.push(error);
+  } else {
+    course.acceptGcseEquivalency = req.session.data.course.acceptGcseEquivalency
+  }
+
+  if (req.session.data.course.acceptGcseEquivalency && req.session.data.course.acceptGcseEquivalency === 'yes') {
+    if (!req.session.data.course.equivalentSubjects) {
+      const error = {};
+      error.fieldName = "gcse-equivalent-subjects";
+      error.href = "#gcse-equivalent-subjects";
+      error.text = "Select which subjects will you accept equivalency tests in";
+      errors.push(error);
+    } else {
+      if (req.session.data.course.equivalentSubjects.includes('english')) {
+        course.acceptEnglishGcseEquivalency = 'yes'
+      }
+
+      if (req.session.data.course.equivalentSubjects.includes('maths')) {
+        course.acceptMathsGcseEquivalency = 'yes'
+      }
+
+      if (req.session.data.course.equivalentSubjects.includes('science')) {
+        course.acceptScienceGcseEquivalency = 'yes'
+      }
+    }
+
+    if (!req.session.data.course.additionalGcseEquivalencies.length) {
+      const error = {};
+      error.fieldName = "gcse-equivalent-details";
+      error.href = "#gcse-equivalent-details";
+      error.text = "Enter details about equivalency tests you offer or accept";
+      errors.push(error);
+    } else {
+      course.additionalGcseEquivalencies = req.session.data.course.additionalGcseEquivalencies
+    }
+  }
+
   if (errors.length) {
     res.render('../views/courses/gcses', {
       organisation,
