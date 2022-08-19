f009bc217f1cefb0125bc667a1842dd7107545da
Simon Whatley <simonwhatley@gmail.com>
2022-08-19 09:45:11 +0100

    Update markdown filter to better parse lists and headings

--- app/filters.js
+++ app/filters.js
@@ -345,8 +345,19 @@ module.exports = (env) => {
     if (!markdown) {
       return null
     }
-    const html = marked.parse(markdown)
-    return html
+
+    const text = markdown.replace(/\\r/g, '\n').replace(/\\t/g, ' ')
+    const html = marked.parse(text)
+
+    // Add govuk-* classes
+    let govukHtml = html.replace(/<p>/g, '<p class="govuk-body">')
+    govukHtml = govukHtml.replace(/<ol>/g, '<ol class="govuk-list govuk-list--number">')
+    govukHtml = govukHtml.replace(/<ul>/g, '<ul class="govuk-list govuk-list--bullet">')
+    govukHtml = govukHtml.replace(/<h2/g, '<h2 class="govuk-heading-l"')
+    govukHtml = govukHtml.replace(/<h3/g, '<h3 class="govuk-heading-m"')
+    govukHtml = govukHtml.replace(/<h4/g, '<h4 class="govuk-heading-s"')
+
+    return govukHtml
   }
 
   /* ------------------------------------------------------------------
