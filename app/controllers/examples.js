fac5b1db5e46442bb485fd6c78045a5624580a04
Simon Whatley <simonwhatley@gmail.com>
2023-03-28 20:30:10 +0100

    Add no-JavaScript flow for adding accredited providers

--- app/controllers/examples.js
+++ app/controllers/examples.js
@@ -1,4 +1,9 @@
+const organisationModel = require("../models/organisations")
 const schoolModel = require("../models/schools")
+
+/// ------------------------------------------------------------------------ ///
+/// SCHOOLS
+/// ------------------------------------------------------------------------ ///
 
 exports.find_school_get = (req, res) => {
   delete req.session.data.school
@@ -176,6 +181,140 @@ exports.edit_school_post = (req, res) => {
         save: `/examples/schools/edit`,
         back: `/examples/schools`,
         cancel: `/examples/schools`,
+      },
+      errors,
+    })
+  } else {
+
+  }
+}
+
+/// ------------------------------------------------------------------------ ///
+/// ACCREDITED PROVIDERS
+/// ------------------------------------------------------------------------ ///
+
+exports.find_accredited_provider_get = (req, res) => {
+  delete req.session.data.school
+
+  res.render("../views/examples/accredited-providers/find", {
+
+    actions: {
+      save: `/examples/accredited-providers/find`,
+      back: `/examples/accredited-providers`,
+      cancel: `/examples/accredited-providers`
+    },
+  })
+}
+
+exports.find_accredited_provider_post = (req, res) => {
+  const errors = []
+
+  if (!req.session.data.accreditedProvider.name.length) {
+    const error = {}
+    error.fieldName = 'accredited-provider'
+    error.href = '#accredited-provider'
+    error.text = 'Enter a provider name, UKPRN or postcode'
+    errors.push(error)
+  }
+
+  if (errors.length) {
+    res.render("../views/examples/accredited-providers/find", {
+
+      actions: {
+        save: `/examples/accredited-providers/find`,
+        back: `/examples/accredited-providers`,
+        cancel: `/examples/accredited-providers`
+      },
+      errors,
+    })
+  } else {
+    res.redirect(`/examples/accredited-providers/choose`)
+  }
+}
+
+exports.choose_accredited_provider_get = (req, res) => {
+  const providers = organisationModel.findMany({
+    isAccreditedBody: true,
+    query: req.session.data.accreditedProvider.name
+  })
+
+  // store total number of results
+  const providerCount = providers.length
+
+  // parse the school results for use in macro
+  let providerItems = []
+  providers.forEach(provider => {
+    const item = {}
+    item.text = `${provider.name} (${provider.code})`
+    item.value = provider.id
+    providerItems.push(item)
+  })
+
+  // sort items alphabetically
+  providerItems.sort((a,b) => {
+    return a.text.localeCompare(b.text)
+  })
+
+  // only get the first 15 items
+  providerItems = providerItems.slice(0,15)
+
+  res.render("../views/examples/accredited-providers/choose", {
+    providerItems,
+    providerCount,
+    searchTerm: req.session.data.accreditedProvider.name,
+    actions: {
+      save: `/examples/accredited-providers/choose`,
+      back: `/examples/accredited-providers/find`,
+      cancel: `/examples/accredited-providers`,
+    },
+  })
+}
+
+exports.choose_accredited_provider_post = (req, res) => {
+  const providers = organisationModel.findMany({
+    isAccreditedBody: true,
+    query: req.session.data.accreditedProvider.name
+  })
+
+  // store total number of results
+  const providerCount = providers.length
+
+  // parse the school results for use in macro
+  let providerItems = []
+  providers.forEach(provider => {
+    const item = {}
+    item.text = `${provider.name} (${provider.code})`
+    item.value = provider.id
+    providerItems.push(item)
+  })
+
+  // sort items alphabetically
+  providerItems.sort((a,b) => {
+    return a.text.localeCompare(b.text)
+  })
+
+  // only get the first 15 items
+  providerItems = providerItems.slice(0,15)
+
+  const errors = []
+
+  if (!req.session.data.school) {
+    const error = {}
+    error.fieldName = 'school'
+    error.href = '#school'
+    error.text = 'Select a school'
+    errors.push(error)
+  }
+
+  if (errors.length) {
+    res.render("../views/examples/accredited-providers/choose", {
+      providerItems,
+      providerCount,
+      searchTerm: req.session.data.accreditedProvider.name,
+      actions: {
+        save: `/examples/accredited-providers/choose`,
+        back: `/examples/accredited-providers/find`,
+        cancel: `/examples/accredited-providers`,
       },
       errors,
     })
