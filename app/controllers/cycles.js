9a81f043622334189058d1c782f32ff0a2e70196
Simon Whatley <simonwhatley@gmail.com>
2024-07-02 12:20:19 +0100

    Use settings data

--- app/controllers/cycles.js
+++ app/controllers/cycles.js
@@ -2,8 +2,10 @@ const organisationModel = require('../models/organisations')
 
 const cycleHelper = require('../helpers/cycles')
 
+const settings = require('../data/settings')
+
 exports.cycle_list = (req, res) => {
-  const isRollover = process.env.IS_ROLLOVER
+  const isRollover = process.env.IS_ROLLOVER || settings.isRollover
 
   if (isRollover === 'true') {
     const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
